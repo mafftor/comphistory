2b6b0c531122c8bcdf4f848c58892e49ad3847e2
Mafftor <mafftor@gmail.com>
2023-02-08 14:28:32 +0200

    Create Symbol validation rule

---
+++ app/Rules/Symbol.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Support\Collection;
+use Illuminate\Contracts\Validation\InvokableRule;
+
+class Symbol implements InvokableRule
+{
+    public function __construct(
+        protected Collection $symbols,
+    ) {}
+
+    /**
+     * Run the validation rule.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     * @return void
+     */
+    public function __invoke($attribute, $value, $fail)
+    {
+        if (!$this->symbols->contains(strtoupper($value))) {
+            $fail('The :attribute does not exist.');
+        }
+    }
+}
